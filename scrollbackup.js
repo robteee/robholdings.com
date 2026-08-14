5471e8b7afbfcffbd546d46e930baaa323cba8a8
robteee <45466400+robteee@users.noreply.github.com>
2026-08-14 13:53:40 -0500

    Create scrollbackup.js
    scroll to top button

---
+++ scrollbackup.js
@@ -0,0 +1,16 @@
+const backToTop = document.querySelector('.back-to-top');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    backToTop.classList.add('show');
+  } else {
+    backToTop.classList.remove('show');
+  }
+});
+
+backToTop.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
